6185e2ec8d6ce67953e75f6be8b021c0eb5f5dfd
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-14 16:15:18 +0300

    Feat: add fonts,icons

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,18 @@
+import './Header.css';
+
+export default function Header() {
+  return (
+    <section className="header">
+      <div className="header__text">
+        <p className="header__subtitle"></p>
+        <p className="header__subtitle"></p>
+        <p className="header__subtitle"></p>
+      </div>
+      <div className="header__icon">
+        <img src="" alt="icon" />
+        <h1 className="header__title"></h1>
+      </div>
+      <img src="" alt="cort" />
+    </section>
+  );
+}
