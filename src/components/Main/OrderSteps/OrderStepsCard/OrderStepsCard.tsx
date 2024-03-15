364c3254bfa68fb1684ba780cc6588f0664a115f
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-15 20:37:32 +0300

    style: ref orderSteps cards

--- src/components/Main/OrderSteps/OrderStepsCard/OrderStepsCard.tsx
+++ src/components/Main/OrderSteps/OrderStepsCard/OrderStepsCard.tsx
@@ -1,23 +1,24 @@
-import './OrderStepsInstruction.css';
+import './OrderStepsCard.css';
 import SubmitBtn from '../../../SubmitBtn/SubmitBtn';
 
-interface OrderStepsInstructionProps {
+interface OrderStepsCardProps {
   title?: string;
   text?: string;
   backgroundColor?: string;
   hoverBackgroundColor?: string;
 }
 
-export default function OrderStepsInstruction({
+export default function OrderStepsCard({
   title,
   text,
   backgroundColor,
   hoverBackgroundColor
-}: OrderStepsInstructionProps) {
+}: OrderStepsCardProps) {
   return (
     <div className="steps-instruction__content">
       <SubmitBtn
         title={title}
+        width="100px"
         backgroundColor={backgroundColor}
         hoverBackgroundColor={hoverBackgroundColor}
       />
