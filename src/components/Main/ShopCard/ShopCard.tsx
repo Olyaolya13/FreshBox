d5ae98f07b89331e10f073f848b5199b4132c270
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-16 12:12:19 +0300

    ref:add cards bestellers

--- src/components/Main/ShopCard/ShopCard.tsx
+++ src/components/Main/ShopCard/ShopCard.tsx
@@ -5,41 +5,72 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import Raspberry from '../../../assets/CardRaspberry.svg';
+
 import Cart from '../../../assets/Cart.svg?react';
 import { ShopCardData } from '../../../utils/constants';
 
 interface ShopCardProps {
   count?: string;
   title?: string;
+  image?: string;
 }
 
-export default function ShopCard({ count, title }: ShopCardProps) {
+export default function ShopCard({ count, title, image }: ShopCardProps) {
   return (
-    <Card sx={{ maxWidth: 250, height: 300, borderRadius: 5, padding: '10px' }}>
+    <Card sx={{ maxWidth: 220, height: 290, borderRadius: 2, padding: '10px' }}>
       <CardMedia
         component="img"
         height="200"
-        image={Raspberry}
+        image={image}
         alt="Raspberry"
-        sx={{ borderRadius: 5 }}
+        sx={{ borderRadius: 2 }}
       ></CardMedia>
       <CardContent sx={{ padding: 0 }}>
-        <Typography gutterBottom variant="h5" sx={{ margin: 0 }}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          sx={{ margin: '5px 0 0 0', fontFamily: 'DMSans', fontSize: '22px', textAlign: 'start' }}
+        >
           {title}
         </Typography>
       </CardContent>
-      <CardActions sx={{ padding: 0, paddingTop: '33px' }}>
-        <Typography gutterBottom component="p" sx={{ padding: 0, margin: 0 }}>
+      <CardActions sx={{ padding: 0, paddingTop: '25px', margin: 0 }}>
+        <Typography
+          gutterBottom
+          component="p"
+          sx={{
+            padding: 0,
+            margin: 0,
+            fontFamily: 'Roboto',
+            color: '#7d7d7d;',
+            fontSize: '18px'
+          }}
+        >
           {count}$
         </Typography>
-        <Button size="small" disableRipple sx={{ display: 'flex', alignItems: 'center' }}>
+        <Button
+          disableRipple
+          size="small"
+          sx={{
+            display: 'flex',
+            alignItems: 'center',
+            paddingLeft: '80px',
+            '&:hover': {
+              backgroundColor: 'transparent'
+            }
+          }}
+        >
           <Cart className="card__icon" />
           <Typography
             gutterBottom
             variant="h5"
             component="div"
-            sx={{ textTransform: 'none', color: '#FF8F52', fontSize: '18px', margin: 0 }}
+            sx={{
+              textTransform: 'none',
+              color: '#FF8F52',
+              fontSize: '15px',
+              margin: 0
+            }}
           >
             {ShopCardData.title}
           </Typography>
