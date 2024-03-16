966531cf1b44953f6bd5b6a800c06600fa483fff
Olga Pankrashina <olyaolya2713@yandex.ru>
2024-03-16 13:01:00 +0300

    ref:add bestelers pagination, red card order

--- src/components/Main/Bestselers/Bestselers.tsx
+++ src/components/Main/Bestselers/Bestselers.tsx
@@ -1,20 +1,13 @@
 import './Bestselers.css';
-import ShopCard from '../ShopCard/ShopCard';
+import BestselersPagination from '../BestelersPagination/BestelersPagination';
 import { BestselersData } from '../../../utils/constants';
-import Raspberry from '../../../assets/СardRaspberry.svg';
-import Blueberry from '../../../assets/СardBlueberry.svg';
-import Mango from '../../../assets/СardMango.svg';
-import Lime from '../../../assets/СardLime.svg';
 
 export default function Bestselers() {
   return (
     <section className="bestselers">
       <h2 className="bestselers__title">{BestselersData.title}</h2>
       <div className="bestselers__card">
-        <ShopCard title="Raspberry" count="5" image={Raspberry} />
-        <ShopCard title="Blueberry" count="5" image={Blueberry} />
-        <ShopCard title="Mango" count="5" image={Mango} />
-        <ShopCard title="Lime" count="5" image={Lime} />
+        <BestselersPagination />
       </div>
     </section>
   );
--- src/components/Main/ShopCard/ShopCard.tsx
+++ src/components/Main/ShopCard/ShopCard.tsx
@@ -5,7 +5,6 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-
 import Cart from '../../../assets/Cart.svg?react';
 import { ShopCardData } from '../../../utils/constants';
 
